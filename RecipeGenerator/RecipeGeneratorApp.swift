a63e2aa60efad6af15c1668f9ed228dde392b6f5
BT_C09_22 <mobileappdev@bths.edu>
2026-04-29 09:01:43 -0400

    Initial Commit

---
+++ RecipeGenerator/RecipeGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeGeneratorApp.swift
+//  RecipeGenerator
+//
+//  Created by Student on 4/29/26.
+//
+
+import SwiftUI
+
+@main
+struct RecipeGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
